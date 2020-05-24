83719c49c49b92bd140866416a6991c951483493
Alex Naish <alex@naish.io>
2020-05-24 12:27:09 +0100

    fix: specify basepath

--- hired/app/index.js
+++ hired/app/index.js
@@ -1,7 +1,5 @@
-console.log('entering into index.js');
-
 import React from 'react';
-import { Switch, Route } from "wouter";
+import { Router, Switch, Route } from "wouter";
 import { render } from "react-dom";
 
 import { SocketProvider } from './components/socketmanager';
@@ -9,15 +7,18 @@ import { Homepage } from './pages/home/';
 import { Gamepage } from './pages/game/';
 import { Userpage } from './pages/user/';
 
+const basePath = process.env.NODE_ENV === 'production' ? '/games/hired' : '/';
+console.log('using basepath', basePath);
 const App = () => (
   <SocketProvider>
-    <Switch>
-      <Route path="/" component={Homepage} />
-      <Route path="/game" component={Gamepage} />
-      <Route path="/user" component={Userpage} />
-    </Switch>
+    <Router base={basePath}>
+			<Switch>
+				<Route path="/" component={Homepage} />
+				<Route path="/game" component={Gamepage} />
+				<Route path="/user" component={Userpage} />
+			</Switch>
+		</Router>
   </SocketProvider>
 );
-console.log('am i here?');
 
 render(<App />, document.getElementById('root'));
