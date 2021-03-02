c1ed81d81a40f5ed58e51710913d176cb6e45bec
Alex Naish <alex@naish.io>
2021-03-02 12:58:19 +0000

    feat: animate in on load

--- sites/hired/pages/index.js
+++ sites/hired/pages/index.js
@@ -1,14 +1,26 @@
-import { useCallback, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
+import { motion } from 'framer-motion';
 
 import Head from 'next/head';
 import Link from 'next/link';
 import Title from '@components/Title';
+
+const variants = {
+	load(index) {
+    return {
+			opacity: 1,
+      x: 100,
+			transition: { delay: index * 0.3, duration: 0.4 }
+    };
+  }
+}
 
 export default function Home() {
   const [channel, setChannel] = useState('');
   const handleChannelChange = useCallback((e) => {
     setChannel(e.target.value);
   });
+
   return (
     <div className="container">
       <Head>
@@ -16,11 +28,17 @@ export default function Home() {
       </Head>
       <main className="content">
         <Title />
-        <Link href="/game/test">New Game</Link>
-        <div>
-          <input type="text" maxLength="4" placeholder="Join Code" onChange={handleChannelChange} />
-          {channel.length === 4 && <Link href={`/join/${channel.toUpperCase()}`}>Join Game</Link>}
-        </div>
+				<motion.div variants={variants} custom={1} animate="load" style={{ opacity: 0, translateX: -100 }}>
+					<Link href="/game/test">New Game</Link>
+				</motion.div>
+				<motion.div variants={variants} custom={2} animate="load" style={{ opacity: 0, translateX: -100 }}>
+					<input type="text" maxLength="4" placeholder="Join Code" onChange={handleChannelChange} />
+				</motion.div>
+				<motion.div variants={variants} custom={3} animate="load" style={{ opacity: 0, translateX: -100 }}>
+					<Link href={channel.length === 4 ? `/join/${channel.toUpperCase()}` : '/#'}>
+						<a disabled={channel.length !== 4}>Join Game</a>
+					</Link>
+				</motion.div>
       </main>
     </div>
   );
